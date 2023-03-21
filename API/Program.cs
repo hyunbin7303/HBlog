4386dedfd0179a35ee43da7aac83d36a42f7b075
kevin <hyunbin7303@gmail.com>
2023-03-21 05:30:15 -0400

    Update Angular side - Http Request/ CORS

--- API/Program.cs
+++ API/Program.cs
@@ -6,8 +6,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
+
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 
 app.MapControllers();
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title: string = 'Project Manager';
+  users: any;
+
+  constructor(private http: HttpClient){
+    
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has been completed.')
+    })
+  }
 }
