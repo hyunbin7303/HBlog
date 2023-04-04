caa527b74c980af3b1009a90ed335bed3e48591c
kevin <hyunbin7303@gmail.com>
2023-04-04 14:49:19 -0400

    Editing user profile from Angular side.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -17,4 +17,7 @@ export class MembersService {
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
+  updateMember(member : Member) {
+    return this.http.put(this.baseUrl + 'users', member);
+  }
 }
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -40,8 +40,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success("Profile updated successfully.");
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success("Profile updated successfully.");
+        this.editForm?.reset(this.member);
+      }
+    })
   }
 }
