2eab82b915f6ddc5a094d8cb44c168728bcffb8b
kevin <hyunbin7303@gmail.com>
2023-04-14 10:13:47 -0400

    Update members.service.ts

--- src/client/src/app/_services/members.service.ts
+++ src/client/src/app/_services/members.service.ts
@@ -12,21 +12,49 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
   // paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>;
-
 
   constructor(private http: HttpClient) { }
   
   getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+    if(response) return of(response);
+
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
     params = params.append('minAge', userParams.minAge);
     params = params.append('maxAge', userParams.maxAge);
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
     
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    )
   }
 
+
+  getMember(username: string) {
+
+    const member = [...this.memberCache.values()]
+        .reduce((arr,elem) => arr.concat(elem.result), [])
+        .find((member:Member) => member.userName == username);
+
+    if(member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+  
+  updateMember(member : Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    )
+  }
   private getPaginatedResult<T>(url: string, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
@@ -42,26 +70,11 @@ export class MembersService {
       })
     );
   }
-  private getPaginationHeaders(pageNumber : number, pageSize: number) {
+  private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
     params = params.append('pageNumber', pageNumber);
     params = params.append('pageSize', pageSize);
     return params;
   }
 
-  getMember(username: string) {
-    const member = this.members.find(x => x.userName == username);
-    if(member) return of(member);
-
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-  
-  updateMember(member : Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index], ...member}
-      })
-    )
-  }
 }
