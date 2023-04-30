a5491ef722cbbfedb4054442ada91a7ff49010f2
kevin <hyunbin7303@gmail.com>
2023-04-30 13:55:44 -0400

    Basic Testing cases

---
+++ src/KevBlog.UnitTests/UserRepositoryTest.cs
@@ -0,0 +1,15 @@
+using KevBlog.Domain.Entities;
+using KevBlog.Domain.Repositories;
+using KevBlog.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace KevBlog.UnitTests
+{
+    public class UserRepositoryTest
+    {
+        public UserRepositoryTest()
+        {
+        }
+    }
+}
--- src/KevBlog.UnitTests/UsersControllerTests.cs
+++ src/KevBlog.UnitTests/UsersControllerTests.cs
@@ -1,16 +1,12 @@
-﻿using AutoFixture;
-using AutoMapper;
+﻿using AutoMapper;
 using KevBlog.Api.Controllers;
 using KevBlog.Application.Automapper;
+using KevBlog.Application.DTOs;
 using KevBlog.Domain.Entities;
 using KevBlog.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace KevBlog.UnitTests
 {
@@ -18,13 +14,12 @@ namespace KevBlog.UnitTests
     {
         private Mock<IUserRepository> _userRepository;
         private Mock<IPostRepository> _postRepository;
-        private Fixture _fixture;
         private UsersController _controller;
         private readonly IMapper _mapper;
         public UsersControllerTests()
         {
-            _fixture= new Fixture();
             _userRepository= new Mock<IUserRepository>();
+            _postRepository= new Mock<IPostRepository>();
             if(_mapper == null)
             {
                 var mappingConfig = new MapperConfiguration(mc =>
@@ -40,25 +35,75 @@ namespace KevBlog.UnitTests
         [Fact]
         public async Task Get_Post_ReturnOk()
         {
-            var postList = _fixture.CreateMany<Post>(3).ToList();
+            // var postList = _fixture.CreateMany<Post>(3).ToList();
 
-            _postRepository.Setup(repo => repo.GetPostsAsync().Result).Returns(postList);
+            // _postRepository.Setup(repo => repo.GetPostsAsync().Result).Returns(postList);
         }
+
         [Fact]
-        public async Task GetUsersAsync_RetrieveData_ReturnOk()
+        public async Task GetUsers_PassValidUserParam_ReturnOk()
         {
-            IEnumerable<User> userList = _fixture.CreateMany<User>(5).ToList();
+            string inputUserName = "kevin0";
+            IEnumerable<User> userList = SampleValidUserData(5);
+            // _userRepository.Setup(repo => repo.Get)
+            // _userRepository.Setup(repo => repo.GetUserLikesQuery)
 
-            _userRepository.Setup(repo => repo.GetUsersAsync().Result).Returns(userList);
 
+        }
+
+        [Fact]
+        public async Task GetUser_PassExistingUserName_ReturnOk()
+        {
+            string inputUserName = "kevin0";
+            IEnumerable<User> userList = SampleValidUserData(1);
+            User returnValue = userList.First();
+            _userRepository.Setup(repo => repo.GetUserByUsernameAsync(inputUserName).Result).Returns(returnValue);
             _controller = new UsersController(_userRepository.Object, _mapper);
 
-            var result = await _controller.GetUser("hyunbin7303");
-            //var obj1 = result as ObjectResult;
-            //Assert.Equal(200, result.)
+            // Act
+            ActionResult<MemberDto> actionResult = await _controller.GetUser(inputUserName);
+
+            // Assertion
+            OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            MemberDto user = Assert.IsType<MemberDto>(okObjectResult.Value);
+            Assert.NotNull(user);
+            Assert.Equal(inputUserName, user.UserName);
         }
 
-        //[Fact]
-        //public async Task GetUserAsync_
+        [Fact]
+        public async Task GetUser_PassNotExistingUser_NotFound()
+        {
+            string inputUserName = "IdontExist";
+            string ExistingUser = "kevin0";
+            IEnumerable<User> userList = SampleValidUserData(1);
+            User returnValue = userList.First();
+            _userRepository.Setup(repo => repo.GetUserByUsernameAsync(ExistingUser).Result).Returns(returnValue);
+            _controller = new UsersController(_userRepository.Object, _mapper);
+
+            // Act
+            ActionResult<MemberDto> actionResult = await _controller.GetUser(inputUserName);
+
+            // Assertion
+            var notFoundObj = Assert.IsType<NotFoundObjectResult>(actionResult.Result);
+            Assert.Equal(404, notFoundObj.StatusCode);
+        }
+
+
+
+        private static IEnumerable<User> SampleValidUserData(int howMany) {
+            List<User> users = new();
+            for(int i=0; i<howMany; i++) {
+                User user = new()
+                {
+                    Id = i,
+                    UserName = "kevin" + i,
+                    KnownAs = "KnownAs" + i,
+                    Gender = "Male",
+                    City = "Kitchener"
+                };
+                users.Add(user);
+            }
+            return users;
+        }
     }
 }
