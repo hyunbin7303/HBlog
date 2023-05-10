0fb37f32b7b37e2509fbf7756428b2bd47298e3e
kevin <hyunbin7303@gmail.com>
2023-05-10 12:23:56 -0400

    Updating ServiceResult struct for service layer return type.

---
+++ src/KevBlog.Application/Common/IResult.cs
@@ -0,0 +1,9 @@
+﻿namespace KevBlog.Application.Common
+{
+    public interface IResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
