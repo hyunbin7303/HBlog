2eeb486c6cb0a24e7cc0cf2fbcd974437aa2bcfb
kevin <hyunbin7303@gmail.com>
2024-02-11 19:25:20 -0500

    Blazorise nuget for markdown - text editor basic setup

--- src/KevBlog.WebClient/Program.cs
+++ src/KevBlog.WebClient/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using KevBlog.WebClient;
 using KevBlog.WebClient.Services;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +11,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorise(options =>
+{
+    options.Immediate = true;
+})
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped<IPostService, PostClientSerivce>();
 builder.Services.AddScoped<UserClientService>();
