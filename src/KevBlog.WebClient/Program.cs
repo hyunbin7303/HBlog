32de69884395d90b4012272d8995fc22c31f9c75
kevin <hyunbin7303@gmail.com>
2024-02-18 12:13:28 -0500

    quick fix for Auth State provider - injecting in program.cs

--- src/KevBlog.WebClient/Program.cs
+++ src/KevBlog.WebClient/Program.cs
@@ -12,10 +12,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped<IPostService, PostClientService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<UserClientService>();
+builder.Services.AddScoped<ApiAuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<ApiAuthStateProvider>());
 
 await builder.Build().RunAsync();
