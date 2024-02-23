f3bed52eaa9dbe5d44e2d377d87d783bbb03490d
kevin <hyunbin7303@gmail.com>
2024-02-23 07:40:17 -0500

    Update RegisterDto.cs
    Fixing for http Request from the blazorclient side.

--- src/KevBlog.Shared/DTOs/RegisterDto.cs
+++ src/KevBlog.Shared/DTOs/RegisterDto.cs
@@ -6,11 +6,6 @@ public class RegisterDto
     [Required] public string FirstName { get; set; }
     [Required] public string LastName { get; set; }
     [Required] public string Email { get; set; }
-    public string KnownAs { get; set; }
-    public string Gender { get; set; }
-    public DateTime? DateOfBirth { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
     [Required]
     [StringLength(8, MinimumLength =4)]
     public string Password { get; set; }
