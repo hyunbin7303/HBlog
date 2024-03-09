83188d6f736f4186e6181fb88847af31fd8d1a62
kevin <hyunbin7303@gmail.com>
2024-03-09 16:04:37 -0500

    Migration code for Post - Category relationship

---
+++ src/KevBlog.Infrastructure/Migrations/20240309210245_CategoryDomainModelFix.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KevBlog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryDomainModelFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostCategories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Posts",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Posts");
+
+            migrationBuilder.CreateTable(
+                name: "PostCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "integer", nullable: false),
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostCategories", x => new { x.CategoryId, x.PostId });
+                    table.ForeignKey(
+                        name: "FK_PostCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostCategories_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostCategories_PostId",
+                table: "PostCategories",
+                column: "PostId");
+        }
+    }
+}
